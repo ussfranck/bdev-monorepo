9a1b546a30219411751539631399a9eb17047cb8
USS Franck <franckmekoulou.dev@hotmail.com>
2025-02-06 23:51:02 +0000

    Fixed detection of Node.js in packaged executables

--- dist/commands/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloCommand = void 0;
-const commander_1 = require("commander");
-exports.helloCommand = new commander_1.Command("hello")
-    .description("Affiche Hello World")
-    .action(() => {
-    console.log("Hello World!");
-});
--- src/commands/check.ts
+++ src/commands/check.ts
@@ -1,21 +1,26 @@
-import { Command } from "commander"
-import { execSync } from "child_process"
+import {Command} from "commander"
+import {execSync} from "child_process"
+import * as os from "os"
+import * as process from "node:process";
 
 export const checkCommand = new Command("check")
-	.description("Vérifie si Node.js et PNPM sont installés")
-	.action(() => {
-		try {
-			const nodeVersion = execSync('node -v', { stdio: 'ignore' }).toString().trim();
-			console.log(`✅ Node.js detected : ${nodeVersion}`)
-		} catch {
-			console.error("❌ Node.js unvailable on your computer")
-			return
-		}
+  .description("Check all information about nodejs environment variables")
+  .action(() => {
+    // @ts-ignore
+    const shellToUse = os.platform() === 'win32' ? 'cmd.exe' : '/bin/sh';
+    try {
+      const nodeVersion = process.version
+      console.log(`✅ Node.js detected : ${nodeVersion}`)
+    } catch(reason) {
+      console.error("❌ Node.js unavailable on your computer")
+      console.log(`Error occurred: ${reason}`)
+      return
+    }
 
-		try {
-			const pnpmVersion = execSync("pnpm -v").toString().trim()
-			console.log(`✅ PNPM détecté : ${pnpmVersion}`)
-		} catch {
-			console.error("❌ PNPM is not installed on your computer")
-		}
-	})
+    try {
+      const pnpmVersion = execSync("pnpm -v", { shell: shellToUse }).toString().trim()
+      console.log(`✅ PNPM detected : ${pnpmVersion}`)
+    } catch {
+      console.error("❌ PNPM is not installed on your computer")
+    }
+  })
