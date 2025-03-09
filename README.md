f70e0fe5e61d56e74e38d816edc2bf23aeb5833a
USS <franckmekoulou.dev@hotmail.com>
2025-03-09 18:26:50 +0000

    init command added to initialize et buil monorepo step by step

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a minimal tool to manage monorepo
 ### Version
-Currently in 1.0.0
+Currently in 0.0.1
--- src/commands/hello.ts
+++
@@ -1,6 +0,0 @@
-import { Command } from "commander"
-export const helloCommand = new Command("hello")
-	.description("Affiche Hello World")
-	.action(() => {
-		console.log("Hello World!")
-	})
